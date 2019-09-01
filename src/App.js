818461b8eece128beb149cdc4040ee574b6d215a
cy <1269903329@qq.com>
2019-09-01 12:28:31 +0800

    修复登录功能的bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Login from './pages/login/login';
 import Admin from './pages/admin/admin';
