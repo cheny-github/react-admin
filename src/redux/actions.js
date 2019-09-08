23c65fa943355764423a171b9a46b70929dd7f02
cy <1269903329@qq.com>
2019-09-08 16:59:00 +0800

    redux基本使用DEMO

---
+++ src/redux/actions.js
@@ -0,0 +1,7 @@
+
+
+export const TYPE_ADD ='add';
+export const add = (number)=>({ type:TYPE_ADD,number });
+
+export const TYPE_SUB = 'sub';
+export const sub = (number)=>({ type:TYPE_SUB,number });
